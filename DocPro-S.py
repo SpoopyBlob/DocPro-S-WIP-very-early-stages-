428a3d7d489500caf2c54f63aa862c9d7b4b2c7f
James Wooff <jamestron360@gmail.com>
2025-03-15 19:14:26 +0000

    Added some functionality so I can start creating redo functionality

--- DocPro-S.py
+++ DocPro-S.py
@@ -7,14 +7,38 @@ import shutil
 import DocPro_Classes as DClass
 
 def create_repos():
-    print("Enter the pathway of the desired location to store your repository.")
-    print("e.g. C://Users//James//Documents\n")
-    path = input("") + "//DocPro-s"
+    path = check_if_first()
+    if path == True:
+        print("Enter the pathway of the desired location to store your repository.")
+        print("e.g. C://Users//James//Documents\n")
+        path = input("") + "//DocPro-s"
+        write_user_path(path)
+
     os.makedirs(path + "//File_Repository", exist_ok=True)
     os.makedirs(path + "//Backup_Repository", exist_ok=True)
-    os.makedirs(path + "//Upload")
+    os.makedirs(path + "//Upload", exist_ok=True)
     os.chdir(path)
+    return path
     
+#Checks if the user has the filepaths
+def check_if_first():
+    #creates file if it dosen't already exist
+    file = open("user_path.txt", "a")
+    file.close()
+
+    file = open("user_path.txt", "r")
+    read = file.read()
+    if len(read) == 0:
+        file.close()
+        return True
+    else:
+        file.close()
+        return read
+    
+def write_user_path(path):
+    file = open("user_path.txt", "w")
+    file.write(path)
+    file.close()
 
 def add_to_active_queue(file_name):
     pass
@@ -69,13 +93,58 @@ def delete_document(filename):
 
 def dupblicate_document(filename, new_filename):
     path = os.getcwd() +"//File_repository//"
-    document = open(path + filename, 'r')
+    document = open(path + filename + ".txt", 'r')
     document_contents = document.read()
     document.close()
 
     path = os.getcwd() +"//File_repository//"
-    with open(path + new_filename, 'w') as dub_file:
+    with open(path + new_filename + ".txt", 'w') as dub_file:
         dub_file.write(document_contents)
+
+#temp function until manifesto is created
+def view_documents(path):
+    print(os.listdir(path + "//File_repository"))
+
+
+#main 
+welcome_message = "Welcome to DocPro-s"
+print(welcome_message)
+print("-" * len(welcome_message) + "\n")
+path = create_repos()
+
+running = True
+while running == True
+    current_files_string = "Current Files:"
+    print(current_files_string + "\n" + ("-" * len(current_files_string)))
+    view_documents(path)
+    print("Add document(a), Remove document (r), Dupblicate_document(d), Create document (c), Exit program (e)")
+    user_input = input("Control: ")
+
+    if user_input.lower() == a:
+        path = input("Enter path:")
+        filename = input("Filename: ")
+        add_document_to_repository(path, filename)
+
+    elif user_input.lower() == r:
+        filename = input("Filename")
+        delete_document()
+    elif user_input.lower() == d:
+        filename = input("Filename: ")
+        new_filename = input("New_filename: ")
+        dupblicate_document()
+    elif user_input.lower() == c:
+        name = input("Name: ")
+        string = input("String: ")
+        create_document()
+    elif user_input.lower() == e:
+        running == False
+
+    else:
+        print("Input ERROR!!! Invalid Input")
+
+
+
+
 
 
 
